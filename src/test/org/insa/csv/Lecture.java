8dbb704827d6a916f31238e66c01e8671b1c1e23
root <root@DESKTOP-6ME6FLB.localdomain>
2018-05-19 09:00:12 +0200

    Modification de Lecture

--- src/test/org/insa/csv/Lecture.java
+++ src/test/org/insa/csv/Lecture.java
@@ -3,78 +3,59 @@ package org.insa.csv;
 import java.io.*;
 import java.util.*;
 
-
+/* Permet de récupérer les informations d'un fichier .txt : */
+/* Nom de la carte, liste des origines, liste des destinations */
 public class Lecture {
-	
+
 	private String mapName ;
 	private ArrayList<Integer> listeOrigine;
 	private ArrayList<Integer> listeDestination;
-	
+
 	public Lecture(String fileName) {
-		listeOrigine = new ArrayList<Integer>();
-		listeDestination = new ArrayList<Integer>();
+		this.listeOrigine = new ArrayList<Integer>();
+		this.listeDestination = new ArrayList<Integer>();
 		LectureFichier (fileName);
-		
 	}
-	
+
 	public void LectureFichier (String fileName) {
 		try {
-            Scanner scan = new Scanner(new File(fileName));
-            if (scan.hasNext()) {
-                mapName = scan.nextLine();
-            }
+			Scanner scan = new Scanner(new File(fileName));
 			int origine;
 			int destination;
+			
+			/* Recupere le nom de la carte */
+			if (scan.hasNext()) {
+				mapName = scan.nextLine();
+			}
+			
+			/* Recupere les origines et destinations */
 			while (scan.hasNextInt()) {
-
 				origine = scan.nextInt();
 				this.listeOrigine.add(origine);
-				 if (scan.hasNextInt()) {
-					 destination = scan.nextInt();
-					 this.listeDestination.add(destination);
-				 } 
-				 else {
-					System.out.println("[WARNING]: ODD AMOUNT OF POINTS");
-				 }
+				if (scan.hasNextInt()) {
+					destination = scan.nextInt();
+					this.listeDestination.add(destination);
+				} 
+				else {
+					System.out.println("[WARNING]: Nombre impair de points");
+				}
 			}
+			scan.close();
 		}
 		catch(FileNotFoundException e) {
-			System.out.println("[EXCEPTION] FILE NOT FOUND");
+			System.out.println(e.getMessage());
 		}
-      //  points = new ArrayList<>();
-        /*try {
-            Scanner scanner = new Scanner(new File(fileName));
-            if (scanner.hasNext()) {
-                mapName = scanner.nextLine();
-            }
-
-            while (scanner.hasNextInt()) {
-                int origin = scanner.nextInt();
-                this.listeOrigine.add(origin);
-                if (scanner.hasNextInt()) {
-                    int destination = scanner.nextInt();
-                    this.listeDestination.add(destination);
-                    //points.add(new Points(origin, destination));
-                } else {
-                    System.out.println("WARNING: odd amount of points");
-                }
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }*/
 	}
-	
+
 	public String getMapName() {
 		return this.mapName;
 	}
 
-	public ArrayList<Integer> getlisteOrigine(){
+	public ArrayList<Integer> getListeOrigine(){
 		return this.listeOrigine;
 	}
-	
-	public ArrayList<Integer> getlisteDestination(){
+
+	public ArrayList<Integer> getListeDestination(){
 		return this.listeDestination;
 	}
 }
--- src/test/org/insa/csv/TestPerformance.java
+++ src/test/org/insa/csv/TestPerformance.java
@@ -6,7 +6,8 @@ public class TestPerformance {
 	
 	@Test
 	public void testLecture() {
-		Lecture lect = new Lecture("/home/katran/Bureau/Maison/Graphes/BE_Graphes/input/haute-garonne.txt");
+		//Lecture lect = new Lecture("/home/katran/Bureau/Maison/Graphes/BE_Graphes/input/haute-garonne.txt");
+		Lecture lect = new Lecture("C:/Users/Alicia/eclipse-workspace/BE_Graphes/input/haute-garonne.txt");
 		System.out.println("ici "+lect.getMapName());
 		System.out.println("ici O "+lect.getlisteOrigine().get(0));
 		
