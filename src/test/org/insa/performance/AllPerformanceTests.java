9d68bf1fd51e2551093ead00e906e11d69fd4001
root <root@DESKTOP-6ME6FLB.localdomain>
2018-05-26 20:26:57 +0200

    Modif tests performance

--- src/test/org/insa/performance/AllPerformanceTests.java
+++ src/test/org/insa/performance/AllPerformanceTests.java
@@ -7,12 +7,12 @@ public class AllPerformanceTests {
 	@Test
 	public void testsPerformance() {
 		TestPerformance test1Temps = new TestPerformance();
-		test1Temps.doRun("C:/Users/Utilisateur/Desktop/BE_Graphes/input/bretagne_15_25.txt", "resultat_performance_bretagne_15_25_temps.csv", 0);
+		test1Temps.doRun("C:/Users/Utilisateur/Desktop/BE_Graphes/input/bretagne_15_25.txt", "resultat_performance_bretagne_15_25_temps_bis.csv", 0);
 		
 		TestPerformance test1Distance = new TestPerformance();
-		test1Distance.doRun("C:/Users/Utilisateur/Desktop/BE_Graphes/input/bretagne_15_25.txt", "resultat_performance_bretagne_15_25_distance.csv", 1);
+		test1Distance.doRun("C:/Users/Utilisateur/Desktop/BE_Graphes/input/bretagne_15_25.txt", "resultat_performance_bretagne_15_25_distance_bis.csv", 1);
 		
-		TestPerformance test2Temps = new TestPerformance();
+		/*TestPerformance test2Temps = new TestPerformance();
 		test2Temps.doRun("C:/Users/Utilisateur/Desktop/BE_Graphes/input/bretagne_50_100.txt", "resultat_performance_bretagne_50_100_temps.csv", 0);
 		
 		TestPerformance test2Distance = new TestPerformance();
@@ -23,6 +23,6 @@ public class AllPerformanceTests {
 		
 		TestPerformance test3Distance = new TestPerformance();
 		test3Distance.doRun("C:/Users/Utilisateur/Desktop/BE_Graphes/input/bretagne_150_170.txt", "resultat_performance_bretagne_150_170_distance.csv", 1);
-	}
+	}*/
 
 }
